8ad1046d0de246da2c013af56332ff8b101f8aa1
Кирилл Максимович <kirillking01@yandex.ru>
2023-11-18 04:43:17 +0300

    style: удалить импорты

--- src/main/java/ru/yandex/practicum/filmorate/Service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/Service/FilmService.java
@@ -2,8 +2,6 @@ package ru.yandex.practicum.filmorate.Service;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.DataNotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
