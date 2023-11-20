a9b5961950fffe7890ec62f9a00c80185f06a14b
Кирилл Максимович <kirillking01@yandex.ru>
2023-11-21 00:26:11 +0300

    refactor: переделать getGenreById c помощью queryForObject

--- src/main/java/ru/yandex/practicum/filmorate/storage/GenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/GenreDbStorage.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
@@ -20,11 +21,12 @@ public class GenreDbStorage {
     }
 
     public Genre getGenreById(int id) {
-        List<Genre> genre = jdbcTemplate.query("SELECT * FROM genre WHERE genre_id = ?", genreRowMapper(), id);
-        if (genre.size() != 1) {
+        try {
+            return jdbcTemplate.queryForObject("SELECT COUNT(*) FROM genre WHERE genre_id = ?",
+                    genreRowMapper(), id);
+        } catch (EmptyResultDataAccessException e) {
             throw new DataNotFoundException("Genre not found: Incorrect id");
         }
-        return genre.get(0);
     }
 
     private RowMapper<Genre> genreRowMapper() {
