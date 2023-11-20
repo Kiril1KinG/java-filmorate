46fcaf096db5c4431f05a77161e5bc7e41b73b4a
Кирилл Максимович <kirillking01@yandex.ru>
2023-11-21 00:34:12 +0300

    refactor: заменить mpaRowMapper под метод-референс

--- src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
@@ -3,10 +3,12 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @ToString
--- src/main/java/ru/yandex/practicum/filmorate/storage/RatingDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/RatingDbStorage.java
@@ -2,11 +2,12 @@ package ru.yandex.practicum.filmorate.storage;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exception.DataNotFoundException;
 import ru.yandex.practicum.filmorate.model.Mpa;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Component
@@ -16,21 +17,22 @@ public class RatingDbStorage {
     private final JdbcTemplate jdbcTemplate;
 
     public List<Mpa> getAllRatings() {
-        return jdbcTemplate.query("SELECT * FROM rating", ratingRowMapper());
+        return jdbcTemplate.query("SELECT * FROM rating", this::mapMpa);
     }
 
     public Mpa getRatingById(int id) {
-        List<Mpa> mpa = jdbcTemplate.query("SELECT * FROM rating WHERE rating_id = ?", ratingRowMapper(), id);
+        List<Mpa> mpa = jdbcTemplate.query("SELECT * FROM rating WHERE rating_id = ?", this::mapMpa, id);
         if (mpa.size() != 1) {
             throw new DataNotFoundException("Rating not found: Incorrect id");
         }
         return mpa.get(0);
     }
 
-    private RowMapper<Mpa> ratingRowMapper() {
-        return ((rs, rowNum) -> new Mpa(
-                rs.getInt("rating_id"),
-                rs.getString("name")
-        ));
+    private Mpa mapMpa(ResultSet rs, int rowNum) throws SQLException {
+        Mpa mpa = new Mpa();
+        mpa.setId(rs.getInt("rating_id"));
+        mpa.setName(rs.getString("name"));
+        return mpa;
+
     }
 }
