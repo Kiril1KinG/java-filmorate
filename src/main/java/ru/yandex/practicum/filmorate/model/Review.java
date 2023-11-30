2ca3b69d7108310a2b76170c706275d2430e6267
Кирилл Максимович <kirillking01@yandex.ru>
2023-11-30 20:31:59 +0300

    feat: создать класс-модель Rewiew

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -0,0 +1,33 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class Review {
+
+    private int id;
+    private String content;
+    private boolean isPositive;
+    private int userId;
+    private int filmId;
+    private int useful;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Review review = (Review) o;
+        return id == review.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
