fc648c050d0c67cb8a85b8c6a835eaf0e0082cf1
Кирилл Максимович <kirillking01@yandex.ru>
2023-12-01 00:38:50 +0300

    fix: переименовать пакет Service и импорты в нижний регистр

--- src/main/java/ru/yandex/practicum/filmorate/controller/ReviewsController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ReviewsController.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.filmorate.controller;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.Service.ReviewService;
+import ru.yandex.practicum.filmorate.service.ReviewService;
 import ru.yandex.practicum.filmorate.model.Review;
 
 import javax.validation.Valid;
