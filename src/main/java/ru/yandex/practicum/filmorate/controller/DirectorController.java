4b89139ae26d5aaa2a0b455d6bb178ed6bd06b2e
RomanBorodulin <djboroda98@yandex.ru>
2023-12-02 01:02:59 +0300

    refactor: folder Service -> service

--- src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.yandex.practicum.filmorate.Service.DirectorService;
+import ru.yandex.practicum.filmorate.service.DirectorService;
 import ru.yandex.practicum.filmorate.model.Director;
 
 import javax.validation.Valid;
