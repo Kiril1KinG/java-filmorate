dbd50d8f354a7b1826d1253bb53488e5a6820651
Райдюк Александр <56934708+alexrad555@users.noreply.github.com>
2023-12-04 15:46:54 +0300

    Общие фильмы

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -23,4 +23,6 @@ public interface FilmStorage {
     void validateFilm(Film film, String operation);
 
     List<Film> getSortedFilmsByDirector(int directorId, List<String> sortBy);
+
+    List<Film> getCommonFilms(int userId, int friendId);
 }
