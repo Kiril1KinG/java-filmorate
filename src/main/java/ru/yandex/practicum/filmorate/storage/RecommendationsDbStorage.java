2284696ed053472af65af787e5d21dd10b818dc8
Райдюк Александр <56934708+alexrad555@users.noreply.github.com>
2023-12-07 22:00:06 +0300

    Доработка рекомендаций после ревью

--- src/main/java/ru/yandex/practicum/filmorate/storage/RecommendationsDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/RecommendationsDbStorage.java
@@ -12,13 +12,21 @@ import java.util.Set;
 @Component
 @Primary
 @RequiredArgsConstructor
-public class RecommendationsDbStorage {
+public class RecommendationsDbStorage implements RecommendationsStorage {
 
     private final JdbcTemplate jdbcTemplate;
     private final LikesExtractor likesExtractor;
 
-    public Map<Integer, Set<Integer>> getAllLikes() {
-        String sql = "select user_id, film_id from \"like\" order by user_id";
-        return jdbcTemplate.query(sql, likesExtractor);
+    @Override
+    public Map<Integer, Set<Integer>> getCommonLikes(int userId) {
+        String sql = "select user_id, film_id from \"like\" " +
+                " where user_id in ( " +
+                "       select l2.user_id " +
+                "         from \"like\" l1 " +
+                "         join \"like\" l2 on l2.film_id = l1.film_id " +
+                "        where l1.user_id = ?" +
+                ")" +
+                "order by user_id";
+        return jdbcTemplate.query(sql, likesExtractor, userId);
     }
 }
